a47adaea8d92c39dbd0809a0e6c6c494eb57d322
Paulo Lopes <paulopes00@hotmail.com>
2018-11-09 15:59:20 -0500

    Change setup.py not to use external files

--- setup.py
+++ setup.py
@@ -8,11 +8,6 @@ from __future__ import print_function, division
 
 from setuptools import setup, find_packages
 
-import os
-
-HERE = os.path.dirname(os.path.realpath(__file__))
-VERSION = open(os.path.join(HERE, 'VERSION')).read().splitlines()[0].strip()
-README = open(os.path.join(HERE, 'README.rst')).read().strip()
 
 setup(
     name='polevault',
@@ -20,12 +15,12 @@ setup(
     author='Paulo Lopes',
     author_email='palopes@cisco.com',
     url='https://paulopes.github.io/polevault/',
-    description='''\
+    description='Encrypts and decrypts credentials',
+    long_description='''\
 Encrypts and decrypts credentials, stores them
 in a .ini, .conf, .json, .yml, or .yaml file,
 and can decrypt them into Hashicorp's Vault.
 ''',
-    long_description=README,
     packages=find_packages(exclude=[
         "*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=[
